c62ea191fd1c1cdf5b55e5fad04e00f32a385b82
muhammad Ashique V <ashiquekavanoor009@gmail.com>
2026-07-15 17:18:01 +0530

    Add command to reset admin password

---
+++ accounts/management/commands/reset_admin_password.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = 'Reset password for the admin superuser'
+
+    def handle(self, *args, **kwargs):
+        username = 'ashique'
+        new_password = 'Chikkundo@123'   # ← Puthu password, ninak orkkan pattunna onnu vekkuka
+
+        try:
+            user = User.objects.get(username=username)
+            user.set_password(new_password)
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Password for "{username}" has been reset successfully.'))
+        except User.DoesNotExist:
+            self.stdout.write(self.style.ERROR(f'User "{username}" not found.'))
